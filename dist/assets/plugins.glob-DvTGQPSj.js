7eba9f7a706d26ca38fc05c3965775a64369d4b8
Eva <evalin1013@gmail.com>
2024-11-15 21:02:04 +0800

    depoly

---
+++ dist/assets/plugins.glob-DvTGQPSj.js
@@ -0,0 +1,1 @@
+const t={vDatePicker:{selectText:"選取",cancelText:"取消",nowButtonText:"現在",today:"今天",yesterday:"昨天",lastWeek:"過去 7 天",lastThirtyDays:"過去 30 天",halfYear:"過去半年",lastMonth:"上個月",thisMonth:"這個月",thisYear:"今年"}};export{t as default};
